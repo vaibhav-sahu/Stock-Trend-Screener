12c6a94c05eb52cd33980aab0468d8346b63a770
Vaibhav Kumar Sahu <96175951+vaibhav-sahu@users.noreply.github.com>
2026-01-07 20:48:44 +0530

    Add files via upload

---
+++ stocktrends/predictor/views.py
@@ -0,0 +1,40 @@
+import json
+from django.shortcuts import render
+from .ml_model import get_stock_data, predict_price, predict_stock
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def result(request):
+    
+    if request.method == 'POST':
+        symbol = request.POST.get('symbol')
+
+        data = get_stock_data(symbol)
+
+        if data is None or data.empty:
+            return render(request, 'index.html', {
+                'error': 'Invalid stock symbol'
+            })
+
+        # Extract close prices safely
+        close_prices = data['Close'].astype(float)
+
+        # Convert index (dates) to strings
+        dates = close_prices.index.strftime('%Y-%m-%d').tolist()
+        prices = close_prices.values.flatten().tolist()
+
+
+        # Limit to last 60 values
+        dates = dates[-60:]
+        prices = prices[-60:]
+
+        prediction = predict_price(data)
+
+        return render(request, 'result.html', {
+            'symbol': symbol.upper(),
+            'prediction': prediction,
+           'dates': json.dumps(dates),     # ✅ FIX
+            'prices': json.dumps(prices)  # ✅ FIX
+        })
